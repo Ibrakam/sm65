b5f3a7bb086c553ec8e12486459d5e7fe74d0372
Ibragim <72756511+Ibrakam@users.noreply.github.com>
2025-09-25 21:01:06 +0500

    dflkjdfg

---
+++ database/postservice.py
@@ -0,0 +1,8 @@
+from database import get_db
+from database.models import UserPost
+
+# Cоздание поста
+# Получение всех постов пользователя - user_id
+# Получение определенного поста - post_id
+# Изменение поста
+# Удаление поста
---
+++ database/userservice.py
@@ -0,0 +1,50 @@
+from database import get_db
+from database.models import User
+
+
+# Функия для добавления пользователя в бд
+def create_user_db():
+    pass
+
+
+# Получение всех или определенного пользователя
+def get_all_or_exact_user(user_id=0):
+    with next(get_db()) as db:
+        if user_id:
+            exact_user = db.query(User).filter_by(id=user_id).first()
+            if exact_user:
+                return exact_user
+            return False
+        all_users = db.query(User).all()
+        return all_users
+
+
+# Удаление пользователя из бд
+def delete_user_db():
+    pass
+
+
+# Изменение пользователя
+def update_user_db(user_id, change_info, new_info):
+    with next(get_db()) as db:
+        to_update_user = db.query(User).filter_by(id=user_id).first()
+        if to_update_user:
+            if change_info == "name":
+                to_update_user.name = new_info
+            elif change_info == "email":
+                to_update_user.email = new_info
+            elif change_info == "password":
+                to_update_user.password = new_info
+            elif change_info == "surname":
+                to_update_user.surname = new_info
+            elif change_info == "city":
+                to_update_user.city = new_info
+            elif change_info == "birthday":
+                to_update_user.birthday = new_info
+            elif change_info == "phone_number":
+                to_update_user.phone_number = new_info
+            elif change_info == "username":
+                to_update_user.username = new_info
+            db.commit()
+            return True
+        return False
