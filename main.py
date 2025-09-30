4b7f95d87297dd81192112661670772dcf4cebb4
Ibragim <72756511+Ibrakam@users.noreply.github.com>
2025-09-30 20:12:53 +0500

    aslkjadsf

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from database import Base, engine
 from api.user.main import user_router
 from api.photo.main import photo_router
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from database.userservice import get_all_or_exact_user
+from database.postservice import all_user_posts
 
-app = FastAPI(docs_url="/")
+templates = Jinja2Templates("templates")
+
+app = FastAPI(docs_url="/docs")
 
 Base.metadata.create_all(engine)
 
@@ -11,6 +17,12 @@ app.include_router(user_router)
 app.include_router(photo_router)
 
 
-@app.get("/wqe")
-async def main():
-    pass
+@app.get("/{uid}", include_in_schema=False, response_class=HTMLResponse)
+async def main(request: Request, uid: int):
+    exact_user = get_all_or_exact_user(user_id=uid)
+    all_posts =
+    return templates.TemplateResponse(request, name="index.html", context={
+                                                                            "user": exact_user
+                                                                        })
+
+# uvicorn main:app --reload
